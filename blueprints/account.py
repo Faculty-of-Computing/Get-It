4f7489a42930243545a2e356f9afa92df7052940
Sito <sitopatrick2004@gmail.com>
2025-08-05 18:08:46 +0100

    Few stylings

--- blueprints/account.py
+++ blueprints/account.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
-from models.or
+# from models.or
 
 blueprint = Blueprint('account', __name__, url_prefix='/account')
 
@@ -8,6 +8,6 @@ blueprint = Blueprint('account', __name__, url_prefix='/account')
 @login_required
 def account():
     # Fetch user orders from the database
-    user_orders = Order.query.filter_by(user_id=current_user.id).order_by(Order.date.desc()).all()
+    # user_orders = Order.query.filter_by(user_id=current_user.id).order_by(Order.date.desc()).all()
     
-    return render_template('account.html', orders=user_orders)
+    return render_template('account.html')
