032b0cdcd9f889c947a4c014932bc570bd606789
Penivera <penivera655@gmail.com>
2025-09-03 11:38:36 +0100

    blagh

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ def create_app():
     # Flask-Mail SMTP config
     app.config['MAIL_SERVER'] = MAIL_SERVER
     app.config['MAIL_PORT'] = MAIL_PORT
-    app.config['MAIL_USE_TLS'] = MAIL_USE_TLS
-    app.config['MAIL_USE_SSL'] = MAIL_USE_SSL
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
     app.config['MAIL_USERNAME'] = MAIL_USERNAME
     app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
     app.config['MAIL_DEFAULT_SENDER'] = MAIL_DEFAULT_SENDER
